511650002ea56ef12dbad33b1f06ab81b70fe5e6
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2021-11-17 19:10:20 +0500

    Second commit

---
+++ src/main/java/io/udevs/apptaskmedical/AppTaskMedicalApplication.java
@@ -0,0 +1,13 @@
+package io.udevs.apptaskmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTaskMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTaskMedicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/udevs/apptaskmedical/AppTaskMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.udevs.apptaskmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTaskMedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
